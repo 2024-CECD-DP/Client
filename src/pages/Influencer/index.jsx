67b9edc02c32683e5fd3af0a2afa060f8b2f9351
Jhcki222 <iqbox2@naver.com>
2024-05-08 20:57:15 +0900

    feat : 메인화면 구현

--- src/pages/Influencer/index.jsx
+++ src/pages/Influencer/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import InfluencerNavbar from '../../components/Navbar/InfluencerNavbar';
 import { styled } from 'styled-components';
-import { Link } from 'react-router-dom';
 
 export default function InfluencerMain() {
     return (
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import { styled } from 'styled-components';
 import { Link } from 'react-router-dom';
-import Navbar from '../../components/Navbar';
 import advertiserBackground from '../../assets/advertiser.png';
 import influencerBackground from '../../assets/influencer.png';
+import LogoImg from '../../assets/Collabo.png';
+import adverIcon from '../../assets/adicon.png';
+import instaIcon from '../../assets/instaicon.png';
+import arrow from '../../assets/ArrowRight.svg';
 
 export default function Main() {
     return (
         <Wrapper>
             <Container>
+                <Logo>
+                    <img src={LogoImg} alt="Logo" />
+                </Logo>
                 <LeftSection>
+                    <p>사업자이신가요?</p>
                     <Link to="/advertiser" style={{ textDecoration: 'none' }}>
-                        <button>사업자 페이지</button>
+                        <button>
+                            <ButtonText>서비스 알아보기</ButtonText>
+                            <ButtonIcon src={adverIcon} alt="광고주아이콘" />
+                            <ColumnLine></ColumnLine>
+                            <Arrow>
+                                <img src={arrow} alt="화살표" />
+                            </Arrow>
+                        </button>
                     </Link>
                 </LeftSection>
                 <RightSection>
+                    <p>인플루언서이신가요?</p>
                     <Link to="/influencer" style={{ textDecoration: 'none' }}>
-                        <button>인플루언서 페이지</button>
+                        <button>
+                            <ButtonText>서비스 알아보기</ButtonText>
+                            <ButtonIcon src={instaIcon} alt="인스타" />
+                            <ColumnLine></ColumnLine>
+                            <Arrow>
+                                <img src={arrow} alt="화살표" />
+                            </Arrow>
+                        </button>
                     </Link>
                 </RightSection>
             </Container>
-            <hr />
-            <h1>Footer</h1>
         </Wrapper>
     );
 }
@@ -40,6 +60,20 @@ const Wrapper = styled.div`
     }
 `;
 
+const Logo = styled.div`
+    display: flex;
+    position: absolute;
+    margin: 15px;
+`;
+
+const ColumnLine = styled.div`
+    display: flex;
+    position: absolute;
+    width: 0px;
+    right: 90px;
+    height: 100%;
+    border: 1px solid var(--black-10, rgba(28, 28, 28, 0.1));
+`;
 const Container = styled.div`
     display: flex;
     width: auto;
@@ -49,7 +83,7 @@ const Container = styled.div`
         display: flex;
         position: relative;
         top: 300px;
-        width: 21.25rem;
+        width: 22.25rem;
         height: 6rem;
         justify-content: center;
         align-items: center;
@@ -68,6 +102,22 @@ const Container = styled.div`
         }
     }
 `;
+
+const ButtonText = styled.span`
+    position: relative;
+    left: -17px;
+`;
+
+const ButtonIcon = styled.img`
+    position: relative;
+    left: -10px;
+`;
+
+const Arrow = styled.div`
+    display: flex;
+    position: relative;
+    left: 20px;
+`;
 const LeftSection = styled.div`
     box-sizing: border-box;
     background-image: url(${advertiserBackground});
@@ -79,6 +129,18 @@ const LeftSection = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-right: -0.6rem;
+    p {
+        color: var(--white-100, #fff);
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        -webkit-text-stroke-width: 1;
+        -webkit-text-stroke-color: var(--white-100, #fff);
+        font-family: Inter;
+        font-size: 64px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 36px; /* 56.25% */
+    }
 `;
 
 const RightSection = styled.div`
@@ -92,4 +154,17 @@ const RightSection = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    border: none;
+
+    p {
+        color: var(--white-100, #fff);
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        -webkit-text-stroke-width: 1;
+        -webkit-text-stroke-color: var(--white-100, #fff);
+        font-family: Inter;
+        font-size: 64px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 36px;
+    }
 `;
