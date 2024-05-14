d28aaa6c33e8b8778504b8094f2a2d1b62ea6dbe
lwef <lin7065@naver.com>
2024-05-14 22:41:53 +0900

    Feat: 검색 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/index';
 import Advertiser from './pages/Advertiser';
 import Influencer from './pages/Influencer';
+import MatchingPage from './pages/Advertiser/MatchingPage';
 
 function App() {
     return (
@@ -11,6 +12,15 @@ function App() {
                 <Route path="/" element={<Main />} />
                 <Route path="/advertiser" element={<Advertiser />} />
                 <Route path="/influencer" element={<Influencer />} />
+                <Route path="/matchingPage" element={<MatchingPage />} />
+                <Route
+                    path = "/*"
+                    element={ 
+                        <div>
+                            <h2>이 페이지는 존재하지 않습니다</h2>
+                        </div>
+                    }
+                />
             </Routes>
         </Router>
     );
